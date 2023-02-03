ebecbeb54f3f33aefe27921ad529d6293b00f91a
Ricardo da Silva Reis <ricardo.reis@olist.com>
2023-02-03 00:00:54 -0300

    Create route account and statement

---
+++ src/index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const { v4: uuidV4 } = require("uuid");
+
+const app = express();
+
+app.use(express.json());
+
+const customers = [];
+
+function verifyIfExistsAccountCPF(request, response, next) {
+  const { cpf } = request.headers;
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if (!customer) {
+    return response.status(400).json({ error: "Customer not Found" });
+  }
+
+  request.customer = customer;
+
+  return next();
+}
+
+app.post("/account", (request, response) => {
+  const { cpf, name } = request.body;
+
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if (customerAlreadyExists) {
+    return response.status(400).json({ error: "Customer Already Exists" });
+  }
+
+  customers.push({
+    cpf,
+    name,
+    id: uuidV4(),
+    statement: [],
+  });
+
+  return response.status(201).send();
+});
+
+app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+  return response.json(customer.statement);
+});
+
+app.listen(3333);
